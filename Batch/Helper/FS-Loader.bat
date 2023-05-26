5be99a7071817bceb401a9e8ec619448791a2c58
ImTimmy228 <74459442+ImTimmy228@users.noreply.github.com>
2023-05-26 07:28:06 +0500

    Update FS-Loader.bat

--- Batch/Helper/FS-Loader.bat
+++ Batch/Helper/FS-Loader.bat
@@ -1,2 +1,4 @@
-timeout /t 2 >nul /nobreak
+@echo off
+
+timeout /t 1 >nul /nobreak
 powershell -WindowStyle hidden -command "Start-Process -FilePath 'Loader.bat' -Verb runas -WindowStyle Maximized"
